ad6b3b3369e48b57dafd4719a858509cad6bf4db
Han Sena <abisena@outlook.com>
2023-12-24 13:51:23 +0700

    Simulating Protocol

--- SpeetTest.playground/Contents.swift
+++ SpeetTest.playground/Contents.swift
@@ -60,6 +60,23 @@ Task {
     }
 }
 
+enum PasswordError: Error {
+    case obvious
+}
+
+func checkPassword(_ password: String) throws -> Bool {
+    if password == "password" {
+        throw PasswordError.obvious
+    }
+    return true
+}
+
+do {
+    try checkPassword("password")
+    print("Password is good!")
+} catch {
+    print("You can't use that password.")
+}
 
 
 
